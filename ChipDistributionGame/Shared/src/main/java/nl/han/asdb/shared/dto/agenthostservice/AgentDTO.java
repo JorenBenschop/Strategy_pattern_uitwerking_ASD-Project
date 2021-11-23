41dea8575c6a63db92f28209e0cdf99e7f7a1df1
Joppe Ruessink <jjw.ruessink@student.han.nl>
2021-11-23 15:35:53 +0100

    Created the DTO for the Agent.

--- ChipDistributionGame/Shared/src/main/java/nl/han/asdb/shared/dto/agenthostservice/AgentDTO.java
+++ ChipDistributionGame/Shared/src/main/java/nl/han/asdb/shared/dto/agenthostservice/AgentDTO.java
@@ -1,4 +1,35 @@
 package nl.han.asdb.shared.dto.agenthostservice;
 
+import nl.han.asdb.shared.dto.agenthostservice.agentrule.AgentRuleDTO;
+
+import java.util.List;
+import java.util.UUID;
+
 public class AgentDTO {
+    private UUID agentId;
+    private List<AgentRuleDTO> agentRules;
+
+    public AgentDTO() {
+    }
+
+    public AgentDTO(UUID agentId, List<AgentRuleDTO> agentRules) {
+        this.agentId = agentId;
+        this.agentRules = agentRules;
+    }
+
+    public UUID getAgentId() {
+        return agentId;
+    }
+
+    public void setAgentId(UUID agentId) {
+        this.agentId = agentId;
+    }
+
+    public List<AgentRuleDTO> getAgentRules() {
+        return agentRules;
+    }
+
+    public void setAgentRules(List<AgentRuleDTO> agentRules) {
+        this.agentRules = agentRules;
+    }
 }
---
+++ ChipDistributionGame/Shared/src/main/java/nl/han/asdb/shared/dto/agenthostservice/agentrule/agentcondition/comparison/AgentComparisonOperatorDTO.java
@@ -0,0 +1,9 @@
+package nl.han.asdb.shared.dto.agenthostservice.agentrule.agentcondition.comparison;
+
+public enum AgentComparisonOperatorDTO {
+    GREATER_THEN,
+    SMALLER_THEN,
+    EQUALS,
+    EQUALS_OR_GREATER_THEN,
+    EQUALS_OR_SMALLER_THEN;
+}
