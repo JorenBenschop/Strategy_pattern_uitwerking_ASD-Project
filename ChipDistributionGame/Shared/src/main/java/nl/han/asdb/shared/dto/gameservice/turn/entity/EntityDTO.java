5ed634de561a44dec0d156ff01b4c75b81ed1360
Maarten Berenschot <mw.berenschot@student.han.nl>
2021-11-25 15:16:37 +0100

    OrderDTO omgezet naar queue

--- ChipDistributionGame/Shared/src/main/java/nl/han/asdb/shared/dto/gameservice/turn/entity/EntityDTO.java
+++ ChipDistributionGame/Shared/src/main/java/nl/han/asdb/shared/dto/gameservice/turn/entity/EntityDTO.java
@@ -1,16 +1,21 @@
 package nl.han.asdb.shared.dto.gameservice.turn.entity;
 
+
 import nl.han.asdb.shared.dto.gameservice.turn.entity.backlog.BacklogDTO;
 import nl.han.asdb.shared.dto.gameservice.turn.entity.delivery.DeliveryDTO;
 import nl.han.asdb.shared.dto.gameservice.turn.entity.order.OrderDTO;
 
+
 import java.util.List;
+import java.util.Queue;
 import java.util.UUID;
+
 
 public class EntityDTO {
 
+
     private UUID entityID;
-    private OrderDTO order;
+    private Queue<OrderDTO> incomingOrders;
     private DeliveryDTO delivery;
     private int currentStock;
     private List<BacklogDTO> backlog;
@@ -22,9 +27,9 @@ public class EntityDTO {
     }
 
 
-    public EntityDTO(UUID entityID, OrderDTO order, DeliveryDTO delivery, int currentStock, List<BacklogDTO> backlog, EntityKind entityKind, EntityLocation entityLocation) {
+    public EntityDTO(UUID entityID, Queue<OrderDTO> incomingOrders, DeliveryDTO delivery, int currentStock, List<BacklogDTO> backlog, EntityKind entityKind, EntityLocation entityLocation) {
         this.entityID = entityID;
-        this.order = order;
+        this.incomingOrders = incomingOrders;
         this.delivery = delivery;
         this.currentStock = currentStock;
         this.backlog = backlog;
@@ -32,57 +37,86 @@ public class EntityDTO {
         this.entityLocation = entityLocation;
     }
 
+
     public UUID getEntityID() {
         return entityID;
     }
+
 
     public void setEntityID(UUID entityID) {
         this.entityID = entityID;
     }
 
-    public OrderDTO getOrder() {
-        return order;
+
+    public Queue<OrderDTO> getIncomingOrders() {
+        return incomingOrders;
     }
 
-    public void setOrder(OrderDTO order) {
-        this.order = order;
+
+    public void setIncomingOrders(Queue<OrderDTO> incomingOrders) {
+        this.incomingOrders = incomingOrders;
     }
+
+
+    public OrderDTO getFirstOrder() {
+        return incomingOrders.poll();
+    }
+
+
+    public OrderDTO peekFirstOrder() {
+        return incomingOrders.peek();
+    }
+
+
+    public void setNewIncomingOrder(OrderDTO order) {
+        incomingOrders.add(order);
+    }
+
 
     public DeliveryDTO getDelivery() {
         return delivery;
     }
 
+
     public void setDelivery(DeliveryDTO delivery) {
         this.delivery = delivery;
     }
+
 
     public int getCurrentStock() {
         return currentStock;
     }
 
+
     public void setCurrentStock(int currentStock) {
         this.currentStock = currentStock;
     }
+
 
     public List<BacklogDTO> getBacklog() {
         return backlog;
     }
 
+
     public void setBacklog(List<BacklogDTO> backlog) {
         this.backlog = backlog;
     }
+
 
     public EntityKind getEntityKind() {
         return entityKind;
     }
 
+
     public void setEntityKind(EntityKind entityKind) {
         this.entityKind = entityKind;
     }
 
+
     public EntityLocation getEntityLocation() {
         return entityLocation;
     }
+
 
     public void setEntityLocation(EntityLocation entityLocation) {
         this.entityLocation = entityLocation;
