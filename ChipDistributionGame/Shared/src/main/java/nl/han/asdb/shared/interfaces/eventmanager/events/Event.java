707c37750caecf3b0abb851f02b9c97db91330f7
Jerdalm <ja.dalm@student.han.nl>
2021-11-26 13:42:37 +0100

    Update Event.java

--- ChipDistributionGame/Shared/src/main/java/nl/han/asdb/shared/interfaces/eventmanager/events/Event.java
+++ ChipDistributionGame/Shared/src/main/java/nl/han/asdb/shared/interfaces/eventmanager/events/Event.java
@@ -1,5 +1,21 @@
 package nl.han.asdb.shared.interfaces.eventmanager.events;
 
-public class Event {
+import java.util.UUID;
 
+public class Event {
+    private UUID eventUuid;
+
+    public Event(){}
+
+    public Event(UUID eventUuid) {
+        this.eventUuid = eventUuid;
+    }
+
+    public UUID getEventUuid() {
+        return eventUuid;
+    }
+
+    public void setEventUuid(UUID eventUuid) {
+        this.eventUuid = eventUuid;
+    }
 }
