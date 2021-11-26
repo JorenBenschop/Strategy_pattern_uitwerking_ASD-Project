ebc133e870fa27729a2c14ff9f6764f24f1d0d32
Rick Jellema <r.jellema@student.han.nl>
2021-11-26 12:50:24 +0100

    whiteline removed.

--- ChipDistributionGame/EventHandler/src/main/java/nl/han/asdb/eventhandler/Main.java
+++ ChipDistributionGame/EventHandler/src/main/java/nl/han/asdb/eventhandler/Main.java
@@ -3,7 +3,6 @@ package nl.han.asdb.eventhandler;
 public class Main {
 
     public static void main(String[] args) {
-        
     }
     public static String test(){
         return "event handler";
