c1e27c3ce450c944f9b8e90429a05f50000c8da4
Rick Jellema <r.jellema@student.han.nl>
2021-11-30 15:49:48 +0100

    Changes to variable name for clarification.

--- ChipDistributionGame/Shared/src/main/java/nl/han/asdb/shared/interfaces/eventmanager/events/game/OrderChipsEvent.java
+++ ChipDistributionGame/Shared/src/main/java/nl/han/asdb/shared/interfaces/eventmanager/events/game/OrderChipsEvent.java
@@ -4,11 +4,10 @@ import nl.han.asdb.shared.interfaces.eventmanager.events.Event;
 
 import java.util.UUID;
 
-import nl.han.asdb.shared.interfaces.eventmanager.events.Event;
 import nl.han.asdb.shared.interfaces.eventmanager.events.EventType;
 
 public class OrderChipsEvent extends Event {
-    private UUID uuid;
+    private UUID buyerUuid;
     private UUID sellerUuid;
     private double amount;
     private double pricePerItem;
@@ -18,9 +17,9 @@ public class OrderChipsEvent extends Event {
         this.eventType = EventType.OrderChipsEvent;
     }
 
-    public OrderChipsEvent(UUID eventUuid, UUID uuid, UUID sellerUuid, double amount, double pricePerItem, short round) {
+    public OrderChipsEvent(UUID eventUuid, UUID buyerUuid, UUID sellerUuid, double amount, double pricePerItem, short round) {
         super(eventUuid);
-        this.uuid = uuid;
+        this.buyerUuid = buyerUuid;
         this.sellerUuid = sellerUuid;
         this.amount = amount;
         this.pricePerItem = pricePerItem;
@@ -28,12 +27,12 @@ public class OrderChipsEvent extends Event {
         this.eventType = EventType.OrderChipsEvent;
     }
 
-    public UUID getUuid() {
-        return uuid;
+    public UUID getBuyerUuid() {
+        return buyerUuid;
     }
 
-    public void setUuid(UUID uuid) {
-        this.uuid = uuid;
+    public void setBuyerUuid(UUID buyerUuid) {
+        this.buyerUuid = buyerUuid;
     }
 
     public UUID getSellerUuid() {
