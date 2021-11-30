6814ca994b5f3c27a03fd779222c7bae512af403
Giovanni <gvds0502@gmail.com>
2021-11-30 14:45:27 +0100

    Every ID is at the top

--- ChipDistributionGame/Shared/src/main/java/nl/han/asdb/shared/dto/DeliveryDTO.java
+++ ChipDistributionGame/Shared/src/main/java/nl/han/asdb/shared/dto/DeliveryDTO.java
@@ -3,7 +3,7 @@ package nl.han.asdb.shared.dto;
 import java.util.UUID;
 
 public class DeliveryDTO {
-    private int amount;
     private UUID receiverId;
+    private int amount;
     private int amountAsked;
 }
