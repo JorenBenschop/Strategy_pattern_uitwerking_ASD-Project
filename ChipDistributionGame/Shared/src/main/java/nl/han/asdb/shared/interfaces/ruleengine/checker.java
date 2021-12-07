59b496820ab3f066f9976e7f09a1c20cbdd5bafd
Joren Benschop <JPP.Benschop@student.han.nl>
2021-12-07 14:47:51 +0100

    - checker.java toegevoegd. Hierin staan lege methodes

---
+++ ChipDistributionGame/Shared/src/main/java/nl/han/asdb/shared/interfaces/ruleengine/checker.java
@@ -0,0 +1,98 @@
+package nl.han.asdb.shared.interfaces.ruleengine;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class checker {
+
+    public boolean checkAgentUuid(UUID agentUuid) {
+
+        return false;
+    }
+
+    public boolean checkPlayerUuid(UUID playerUuid) {
+
+        return false;
+    }
+
+    public boolean checkBuyerUuid(UUID buyerUuid) {
+
+        return false;
+    }
+
+    public boolean checkSellerUuid(UUID sellerUuid) {
+
+        return false;
+    }
+
+    public boolean checkRecipientUuid(UUID recipientUuid) {
+
+        return false;
+    }
+
+    public boolean checkSenderUuid(UUID senderUuid) {
+
+        return false;
+    }
+
+    public boolean checkEntityUuid(UUID entityUuid) {
+
+        return false;
+    }
+
+    public boolean checkGameUuid(UUID gameUuid) {
+
+        return false;
+    }
+
+    public boolean checkPeerUuid(UUID peerUuid) {
+
+        return false;
+    }
+
+    public boolean checkPlayerIp(String ownIp){
+
+        return false;
+    }
+
+    public boolean checkPeerIp(String peerIp){
+
+        return false;
+    }
+
+    public boolean checkBacklogValidity(short backlog){
+
+        return false;
+    }
+
+    public boolean checkRoundValidity(short round){
+
+        return false;
+    }
+
+    public boolean checkOrderAmountValidity(short orderAmount){
+
+        return false;
+    }
+
+    public boolean checkValidMessage(String message){
+
+        return false;
+    }
+
+    public boolean checkTimeDate(LocalDateTime TimeDate){
+
+        return false;
+    }
+
+    public boolean checkChanceEventWithinBounds(double chanceValue) {
+
+        return false;
+    }
+
+    public boolean checkCardExists(String card){
+
+        return false;
+    }
+
+}
