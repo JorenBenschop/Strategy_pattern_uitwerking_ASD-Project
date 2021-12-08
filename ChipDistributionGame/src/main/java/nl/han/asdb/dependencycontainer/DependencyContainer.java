c60e5de0c4f224edba2e5f8213202cc2cdfb270f
Rick Jellema <r.jellema@student.han.nl>
2021-12-08 12:09:20 +0100

    DependencyContainer for main project.
    DependencyContainer in main project so that every module can access dependencies.

---
+++ ChipDistributionGame/src/main/java/nl/han/asdb/dependencycontainer/DependencyContainer.java
@@ -0,0 +1,11 @@
+package nl.han.asdb.dependencycontainer;
+
+import com.google.inject.AbstractModule;
+
+public class DependencyContainer extends AbstractModule {
+    @Override
+    protected void configure() {
+        // bindings here.
+        //bind(ITesTKlasse.class).to(TestKlasse.class);
+    }
+}
