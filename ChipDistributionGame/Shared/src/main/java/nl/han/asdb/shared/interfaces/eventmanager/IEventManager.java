24279ee333b6c8ba0c52c8eaf01d01ff876d13cd
Rick Jellema <r.jellema@student.han.nl>
2021-12-08 09:22:49 +0100

    changes to interface eventhandler and mocked implementation.

--- ChipDistributionGame/Shared/src/main/java/nl/han/asdb/shared/interfaces/eventmanager/IEventManager.java
+++ ChipDistributionGame/Shared/src/main/java/nl/han/asdb/shared/interfaces/eventmanager/IEventManager.java
@@ -4,9 +4,11 @@ import nl.han.asdb.shared.interfaces.eventmanager.events.Event;
 import nl.han.asdb.shared.interfaces.eventmanager.events.EventType;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public interface IEventManager {
     void subscribe(EventType event, IListener listener);
     void unsubscribe(EventType event, IListener listener) throws EventNotFoundException;
     void notify(Event event);
+    HashMap<EventType, ArrayList<IListener>> getListenersForEvents();
 }
