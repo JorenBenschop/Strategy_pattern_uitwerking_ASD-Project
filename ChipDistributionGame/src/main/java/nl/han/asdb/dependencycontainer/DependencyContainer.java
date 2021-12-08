782e61dfaa73654b341e6b1ebbf3528bdd234578
Rick Jellema <r.jellema@student.han.nl>
2021-12-08 12:16:06 +0100

    Only commented bindings currently.
    Commented bindings currently because in shared some of these things do not exist in this branch.

--- ChipDistributionGame/src/main/java/nl/han/asdb/dependencycontainer/DependencyContainer.java
+++ ChipDistributionGame/src/main/java/nl/han/asdb/dependencycontainer/DependencyContainer.java
@@ -1,11 +1,14 @@
 package nl.han.asdb.dependencycontainer;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Singleton;
+import nl.han.asdb.shared.interfaces.eventmanager.IEventManager;
 
 public class DependencyContainer extends AbstractModule {
     @Override
     protected void configure() {
         // bindings here.
+        //bind(IEventManager.class).to(EventManager.class).in(Singleton.class);
         //bind(ITesTKlasse.class).to(TestKlasse.class);
     }
 }
